a0fa5e8e18b160bbaecde94cf4995259348df8a1
wiserubi <wiserubi@gmail.com>
2017-10-14 16:32:46 +0900

    회원가입및 수정삭제, 모달예제

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -8,10 +8,25 @@
  * Controller of the angularJsexamApp
  */
 angular.module('angularJsexamApp')
-  .controller('SignupCtrl', function () {
+  .controller('SignupCtrl', [
+  "Data", "$scope", "$state",
+   function (Data, $scope, $state) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+    $scope.name = "";
+    $scope.age = "";
+    $scope.saveUserInfo = function() {
+    	var dataPromise = Data.setData(
+    		'http://127.0.0.1:52273/user',
+    		'&name='+$scope.name+'&age='+$scope.age);
+    	dataPromise.then(function(restuls){
+    		$scope.name = "";
+    		$scope.age = "";
+    	},function(reason){},function(update){});
+    };
+
+  }]);
--- app/scripts/controllers/user-list.js
+++ app/scripts/controllers/user-list.js
@@ -8,10 +8,51 @@
  * Controller of the angularJsexamApp
  */
 angular.module('angularJsexamApp')
-  .controller('UserListCtrl', function () {
+  .controller('UserListCtrl', [
+  	"Data", "$scope", "$state", 
+  	function (Data, $scope, $state) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+    //페이지가 로딩되었을 때 호출
+    $scope.$on('$viewContentLoaded', function() {
+    	$scope.requestUserList();
+    });
+    $scope.userList = [];
+    $scope.requestUserList = function() {
+    	var dataPromise = Data.getData(
+    		'http://127.0.0.1:52273/user');
+    	dataPromise.then(function(results) {
+    		$scope.userList = results.data;
+    	},function(reason){},function(update){});
+    }
+
+    $scope.deleteUserInfo = function(id) {
+    	var dataPromise = Data.deleteData(
+    		'http://127.0.0.1:52273/user/'+id, '');
+    	dataPromise.then(function(results) {
+    		$scope.requestUserList();
+    	},function(reason){},function(update){});
+    }
+
+    $scope.modifyUserInfo = function(id,name,age) {
+    	var dataPromise = Data.modifyData(
+    		'http://127.0.0.1:52273/user/'+id, 
+    		'&name='+name+'&age='+age);
+    	dataPromise.then(function(results) {
+    		$scope.requestUserList();
+    	},function(reason){},function(update){});
+    }
+    
+    $scope.userInfo={};
+    $scope.getUserInfo= function(id) {
+      var dataPromise = Data.getData(
+        'http://127.0.0.1:52273/user/'+id);
+      dataPromise.then(function(results) {
+        $scope.userInfo = results.data;
+      },function(r){},function(u){});
+    }
+
+  }]);
